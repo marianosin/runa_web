29a148572f286a8f04cf6965183e376a71d3d96a
Mariano Radusky <marianoradusky.19@gmail.com>
2022-03-30 18:53:01 -0300

    Elimino elementos para empexar desafio de counter

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -5,24 +5,13 @@ import CardNewProducts from './CardNewProducts'
 
 export default function ItemListContainer({greetings}){
 
-    let productList = ["Collar", "Pulsera", "Prendedor"]
-    function delProd() {
-        alert("Holas")
-    }
+    
     
    return (
 
-    <div className='Novedades'>
-
-        <h2>Novedades</h2>
-        <CardGroup>
-            {productList.map((value)=>{
-                return (
-                    <CardNewProducts products={value} delProd={delProd} />
-                )
-            })}
-        </CardGroup>
-    </div>
+        <div>
+            Aquí construiré la botonera
+        </div>
 
 
    )
