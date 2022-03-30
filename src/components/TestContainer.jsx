1b6062ff380c4792322107d16eb048545c58f309
Mariano Radusky <marianoradusky.19@gmail.com>
2022-03-30 15:12:36 -0300

    formateo para diferenciar

--- src/components/TestContainer.jsx
+++ src/components/TestContainer.jsx
@@ -9,7 +9,10 @@ export default function TestContainer(){
    return (
 
         <>
+            <h2 style={{marginTop: '2%'}}>
             Test para clase
+            </h2>
+            
         </>
 
 
