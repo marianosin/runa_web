2acbf2d36449ccbad77a6f288e1ccff7a8464b50
Mariano Radusky <marianoradusky.19@gmail.com>
2022-03-30 14:27:13 -0300

    Se elimina componente de ejemplo

--- src/components/Saludar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Saludar({name, lastname}) {
-    let fullName = name + " " + lastname
-    return (
-      <p>Hola {fullName}, ¿Cómo estás?</p>
-    )
-  
-}
