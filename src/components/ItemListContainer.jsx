9dd4b375fc4120b1f4fcf986cd9994964b676bc2
Mariano Radusky <marianoradusky.19@gmail.com>
2022-03-31 16:15:54 -0300

    se llama al counter

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,16 +1,19 @@
 //This component is indended to be a grid ofproducts
 
+import { useState } from 'react'
 import { CardGroup} from 'react-bootstrap'
 import CardNewProducts from './CardNewProducts'
-
+import ItemCount from './ItemCount'
 export default function ItemListContainer({greetings}){
 
+    //let product = {"name":"collar", "stock":100, "price":2300}
     
-    
+    let [product, productController] = useState({"name":"collar", "stock":10, "price":2300, "q":0})
+    console.log(product)
    return (
 
         <div>
-            Aquí construiré la botonera
+            <ItemCount product={product} productController={productController} />
         </div>
 
 
