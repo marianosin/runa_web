fee3bb615f8eb834c7a8de1fd2325956bfa8d376
Mariano Radusky <marianoradusky.19@gmail.com>
2022-04-07 19:23:12 -0300

    corrije nombre

---
+++ src/components/utils/products.js
@@ -0,0 +1,14 @@
+const products = [
+    {
+        id: 1,
+        name: "Collar",
+        stock: 10,
+        price: 6200.00
+    },
+    {
+        id: 2,
+        name: "Aros",
+        stock: 112,
+        price: 3400.00
+    }
+]
