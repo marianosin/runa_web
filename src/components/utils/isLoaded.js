a1e2bb4b9706949e5e63dd0e8614e01dc0e94470
Mariano Radusky <marianoradusky.19@gmail.com>
2022-04-08 09:08:50 -0300

    Añado explicación de Promise

--- src/components/utils/isLoaded.js
+++ src/components/utils/isLoaded.js
@@ -1,5 +1,12 @@
 let isOk = true;
+/*
+This is a Promise. When the server sends info (in tjis case represented by isOk)
+this is executed. In the case that something goes wrong, else is executed and sends a
+message to the console.
+Inside the Promise we han say what jas to be done or what conditions we want.
+In this case we tell the app to wait for a specified time.Añado explicación de Promise
 
+*/
 const isLoaded = (time,task) =>{
     return new Promise((resolve, reject)=>{
         setTimeout(()=>{
