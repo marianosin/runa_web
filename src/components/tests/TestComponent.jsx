514217dbf3e39a12fe8b6a45a714ffca99983682
Mariano Radusky <marianoradusky.19@gmail.com>
2022-04-10 17:08:46 -0300

    Test no relevante

--- src/components/tests/TestComponent.jsx
+++ src/components/tests/TestComponent.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
-
-export default function TestComponent({universities}) {
+import {Card, Button} from 'react-bootstrap'
+export default function TestComponent({id, name, stock, price,}) {
   return (
     <div>
-      <h3>Test component</h3>
-      {universities.map(item=>{
-        return(
-          <div>
-            <div style={{backgroundColor:"blue"}}>
-              {item.name}
-            </div>
-            <div style={{backgroundColor: "green"}}>
-              {item.country}
-            </div>
-          </div>
-        )
-      })}
+                    <Card key={id} className="mainCardSize cardStyle">
+                
+                <Card.Body className='cardBody'>
+                <Card.Title>{name} </Card.Title>
+                <Card.Img variant="top" src="https://joyasnehgne.cl/wp-content/uploads/2019/10/IMG-3508.jpg" />
+                <Card.Text>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto mollitia, error ut in nulla hic fuga nesciunt? In quod eaque consequuntur, incidunt obcaecati iure maiores?
+                    <p>En stock: {stock} unidades </p>
+                </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                <Card.Text>
+                    Precio: ARS {price}
+                </Card.Text>
+                <Card.Text>
+                    {/*<ItemCount stock={stock}/>*/}
+                </Card.Text>
+                <Button  variant="primary">Agregar a carrito</Button>
+                </Card.Footer>
+            </Card>
      </div>
   )
 }
