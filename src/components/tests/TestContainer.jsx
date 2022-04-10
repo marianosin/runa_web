12d4ec52af6b191845eab89641ca245023331c39
Mariano Radusky <marianoradusky.19@gmail.com>
2022-04-10 12:47:51 -0300

    Asigno resultado a estado y se lo mando a componente

--- src/components/tests/TestContainer.jsx
+++ src/components/tests/TestContainer.jsx
@@ -1,13 +1,15 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import TestComponent from './TestComponent'
 /* This component is to create testing apps without changing real components of this app*/
 
 export default function TestContainer() {
 
+    const [universities, setUniversities] = useState([]);
+
     useEffect(() => {
         const requestOptions = {
             method: 'GET',
-            body: "",
+            
             redirect: 'follow'
           };
           
@@ -15,7 +17,8 @@ export default function TestContainer() {
             .then(response => response.json())
             .then(result => {
                 //This is what the promise will do if everything is ok
-                console.log(result)
+
+                setUniversities(result)
             })
             .catch(error => console.log('error', error));
     
@@ -28,7 +31,7 @@ export default function TestContainer() {
 
   return (
     <div>
-        <TestComponent />  
+        <TestComponent universities={universities} />  
     </div>
   )
 }
