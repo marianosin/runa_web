e39afc4f531ff3a2d8a97d167b9cdd1f827276ad
Mariano Radusky <marianoradusky.19@gmail.com>
2022-04-10 12:42:32 -0300

    Se diseña un fetch básico de respuesta

--- src/components/tests/TestContainer.jsx
+++ src/components/tests/TestContainer.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import TestComponent from './TestComponent'
+/* This component is to create testing apps without changing real components of this app*/
 
 export default function TestContainer() {
+
+    useEffect(() => {
+        const requestOptions = {
+            method: 'GET',
+            body: "",
+            redirect: 'follow'
+          };
+          
+          fetch("http://universities.hipolabs.com/search?country=United+States", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                //This is what the promise will do if everything is ok
+                console.log(result)
+            })
+            .catch(error => console.log('error', error));
+    
+      return () => {
+        
+      }
+    }, [])
+    
+
+
   return (
     <div>
         <TestComponent />  
