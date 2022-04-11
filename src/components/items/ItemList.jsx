ffdc85d1fa3f0bcf15c68f8a82f52961ffcb215a
Mariano Radusky <marianoradusky.19@gmail.com>
2022-04-11 20:26:56 -0300

    añado link dinamico a páginn de detalles

--- src/components/items/ItemList.jsx
+++ src/components/items/ItemList.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import Item from './Item'
-
+import { Link } from 'react-router-dom'
 export default function ItemList({products}) {
     
   return (
     products.map(p =>(
       //Item contains a card that recives these parameters
-        <Item
-            key= {p.id}
-            name= {p.name}
-            price= {p.price}
-            stock={p.stock}  />
+        <div>
+          <Link to={`/dynamicelement/${p.id}`}>
+            <Item
+                key= {p.id}
+                name= {p.name}
+                price= {p.price}
+                stock={p.stock}  />
+          </Link>
+        </div>
+
     ))
     
   )
