a3ef485cf13987f16d85fa334093525dd463235b
Mariano Radusky <marianoradusky.19@gmail.com>
2022-04-11 20:42:23 -0300

    se añade el id del item

--- src/components/items/ItemList.jsx
+++ src/components/items/ItemList.jsx
@@ -6,15 +6,16 @@ export default function ItemList({products}) {
   return (
     products.map(p =>(
       //Item contains a card that recives these parameters
-        <div>
-          <Link to={`/dynamicelement/${p.id}`}>
+        
+          
             <Item
                 key= {p.id}
+                id = {p.id}
                 name= {p.name}
                 price= {p.price}
                 stock={p.stock}  />
-          </Link>
-        </div>
+          
+        
 
     ))
     
