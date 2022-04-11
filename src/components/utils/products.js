2789fd68768d300f0f29bac9c9738e857c8f94cb
Mariano Radusky <marianoradusky.19@gmail.com>
2022-04-11 20:10:53 -0300

    Se da un formato completo a los obetos

--- src/components/utils/products.js
+++ src/components/utils/products.js
@@ -1,16 +1,24 @@
 const products = [
     {
         id: 1,
-        name: "Collar",
-        stock: 10,
-        price: 6200.00
+        category: "prendedores",
+        image:"https://http2.mlstatic.com/D_NQ_NP_916611-MLA49335814997_032022-O.webp",
+        name: "Prendedor de plata",
+        stock: 13,
+        price: 2900,
+        description: "Prendedor de plata de alta calidad. Se elabora a mano por productores locales."
     },
     {
         id: 2,
-        name: "Aros",
-        stock: 112,
-        price: 3400.00
+        category: "Otro producto",
+        image:"https://http2.mlstatic.com/D_NQ_NP_916611-MLA49335814997_032022-O.webp",
+        name: "Prendedor de oro",
+        stock: 13,
+        price: 2900,
+        description: "Prendedor de plata de alta calidad. Se elabora a mano por productores locales."
     }
+    
+    
 ]
 export default products
 
