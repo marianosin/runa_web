284df6c95c9a6c5fa2f98ffb9a8e2ba61a81361f
Mariano Radusky <marianoradusky.19@gmail.com>
2022-04-11 20:11:45 -0300

    se le incorpora el parámetro para que se le pueda pasar datos

--- src/components/items/ItemDetailContainer.jsx
+++ src/components/items/ItemDetailContainer.jsx
@@ -2,20 +2,20 @@ import React, { useState, useEffect } from 'react'
 import loadsDetails from '../utils/loadsDetails'
 import ItemDetail from './ItemDetail'
 import {detailTestProduct} from '../utils/products'//Takes test product
-export default function ItemDetailContainer() {
+export default function ItemDetailContainer({product}) {
 
     //This creates a state
     const [item, setItem] = useState({})
 
     useEffect(() => {
         //Runs in third place setting state as detailTestProduct
-      loadsDetails(2000, detailTestProduct)
+      loadsDetails(2000, product)
       .then(res=>{
           setItem(res) //Here we should call setItem
       })
       .catch(error=> console.log(error))
       
-    }, [])
+    }, [product])
     console.log(item)//Runs first before setting state
 
   return (
