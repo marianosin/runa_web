1f3661f58abfd3eb23307893e8007e70b9efecb0
Mariano Radusky <marianoradusky.19@gmail.com>
2022-04-12 08:19:22 -0300

    Ruta category all

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import TestContainer from './components/tests/TestContainer';
 import ItemDetailContainer from './components/items/ItemDetailContainer';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import DynamicElement from './components/items/CategoryContainer';
+import CategoryContainer from './components/items/CategoryContainer';
 //Main comp. Here we mount all components to be rendered
 function App() {
 
@@ -25,7 +26,7 @@ function App() {
             <Route exact path='/' element={<ItemListContainer  />}/>
             <Route exact path='/productdetail' element={<TestContainer  />}/>
             <Route exact path='/nosotros' element={<div>Página test para probar router</div>}/>
-            <Route exact path='/category/all' element={<div>Aquí se presentarán todas las categorías juntas</div>}/>
+            <Route exact path='/category/all' element={<CategoryContainer/>}/>
             <Route exact path='/category/:category/product/:id' element={<DynamicElement  />}/>
           </Routes>   
           
