34f56f501e013132ccdac128648d3afaa6aea76f
Mariano Radusky <marianoradusky.19@gmail.com>
2022-04-18 20:26:56 -0300

    Se mapean los resultados de la categoria

--- src/components/items/CategoryContainer.jsx
+++ src/components/items/CategoryContainer.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 import products from '../utils/products'
 import CategoryList from './CategoryList'
+import Item from './Item'
 
 export default function CategoryContainer() {
-    let categoryList = products.map(p => p.category)
+  const {category} = useParams()
+  const categoryProducts = products.filter(p => p.category === category)
   return (
-    <div>
-        <CategoryList categoryList={categoryList}/>
+    <div style={{display: "flex"}}>
+        {categoryProducts.map((p) =>(
+          <Item
+          key= {p.id}
+          category={p.category}
+          id = {p.id}
+          name= {p.name}
+          image = {p.image}
+          description= {p.description}
+          price= {p.price}
+          stock={p.stock}  />))}
     </div>
   )
 }
