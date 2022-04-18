0c7c8b264456dd568d5b17a215d322f4781a825e
Mariano Radusky <marianoradusky.19@gmail.com>
2022-04-18 20:26:35 -0300

    Se añaden productos diferentes de ejemplo

--- src/components/utils/products.js
+++ src/components/utils/products.js
@@ -6,16 +6,34 @@ const products = [
         name: "Prendedor de plata",
         stock: 13,
         price: 2900,
-        description: "Prendedor de plata de alta calidad. Se elabora a mano por productores locales."
+        description: "Descripcion de producto 1"
     },
     {
         id: 2,
         category: "otros",
         image:"https://http2.mlstatic.com/D_NQ_NP_916611-MLA49335814997_032022-O.webp",
         name: "Prendedor de oro",
-        stock: 13,
+        stock: 16,
         price: 2900,
-        description: "Prendedor de plata de alta calidad. Se elabora a mano por productores locales."
+        description: "Descripción del producto 2"
+    },
+    {
+        id: 3,
+        category: "collares",
+        image:"https://http2.mlstatic.com/D_NQ_NP_916611-MLA49335814997_032022-O.webp",
+        name: "Collares de plata",
+        stock: 116,
+        price: 6900,
+        description: "Descripción del producto 3"
+    },
+    {
+        id: 4,
+        category: "otros",
+        image:"https://http2.mlstatic.com/D_NQ_NP_916611-MLA49335814997_032022-O.webp",
+        name: "Producto 4",
+        stock: 6,
+        price: 200,
+        description: "Descripción del producto 4"
     }
     
     
