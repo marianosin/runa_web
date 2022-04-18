a2829e812edff00dbecff6e11af5d506ed4b65df
Mariano Radusky <marianoradusky.19@gmail.com>
2022-04-18 20:26:16 -0300

    Se añade lógica de map a categorias

---
+++ src/components/navbar/NavContainer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import products from '../utils/products'
+import NavbarComp from './NavbarComp'
+import { useEffect, useState } from 'react'
+import isLoaded from '../utils/isLoaded'
+export default function NavContainer() {
+    const [cat, setCat] = useState([])
+    useEffect(() => {
+      isLoaded(2000, products)
+      .then(result => setCat(result))
+      .catch(error =>console.log(error))
+    
+      
+    }, [cat])
+    
+  return (
+    <NavbarComp categories={cat}/>
+  )
+}
