4236966a1625743b98610cdd3ff0b9ef1aa400bf
Mariano Radusky <marianoradusky.19@gmail.com>
2022-04-26 07:59:13 -0300

    Incorporo algunos estilos

--- src/components/items/Item.jsx
+++ src/components/items/Item.jsx
@@ -1,27 +1,60 @@
 import React from 'react'
-import {Card, Button} from 'react-bootstrap'
-import ItemCount from './ItemCount'
-import { Link } from 'react-router-dom'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import {  CardActionArea, CardActions } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
+import { cardActionAreaSx, itemCardSx } from './style/itemCardSx';
+import {Link} from 'react-router-dom'
+import { useContext, useState } from 'react';
+import { CartContext } from '../cart/CartContext';
 export default function Item({id, category, name, stock, description, price, image}) {
+  const {addToCart, cart, setCart} = useContext(CartContext)
+  const [stop, setStop] = useState(false)
+  function handleClick(){
+    
+      let quantity = 1
+      
+      while (stop !== true) {
+        addToCart({id, name, quantity, price, image, cart, setCart})
+        setStop(true)
+      }
+      
+      
+
+}
+  
   return (
     
-    <Card key={id} className="mainCardSize cardStyle" style={{border: 'none'}}>
-    <Link to={`/category/${category}/product/${id}`}  >          
-                <Card.Body className='cardBody'>
-                <Card.Title>{name} </Card.Title>
-                <Card.Img variant="top" src={image} />
-                <Card.Text>
-                    <p>{description} </p>
-                    <p>En stock: {stock} unidades </p>
-                    <p>Precio:{price} </p>
-                    
-                </Card.Text>
-
-                </Card.Body>
-                
-                </Link>
-            </Card>
-        
+    <Card sx={itemCardSx } key={id}  >
+      {/*<Link to={'/category/collares'} style={{color: 'none', textDecoration: 'none'}} >*/}
+      <CardActionArea sx={cardActionAreaSx} component={Link} to={`/category/${category}/product/${id}`} >
+        <CardMedia
+          component="img"
+          height="140"
+          image={image}
+          alt={name}
+        />
+        <CardContent >
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      {/*</Link>*/}
+     
+      <CardActions>
+        <IconButton color="secondary" aria-label="add to shopping cart">
+            <AddShoppingCartIcon onClick={handleClick} />
+        </IconButton>
+      </CardActions>
+    </Card>
 
   )
 }
---
+++ src/components/items/style/itemCardSx.js
@@ -0,0 +1,16 @@
+let itemCardSx = { maxWidth: 340,
+    margin: '2%',
+    borderRadius: '30px',
+    boxShadow: '0px 0px 10px rgba(0,0,0,0.5)',
+    backgroundColor: '#cc99ff'
+
+}
+let cardActionAreaSx = {
+    transition: "linear 0.2s",
+    '&:hover': {
+        transform: 'scale(1.1)',
+        color: '#fff'
+         }
+}
+
+export {itemCardSx, cardActionAreaSx}
